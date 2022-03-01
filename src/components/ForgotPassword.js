7f80e81f76b68d83704c84e70143d5e8908a6bec
Michael Ndon <manj19.mjn@gmail.com>
2022-03-01 18:28:53 +0100

    style: material UI components on forgot password

--- src/components/ForgotPassword.js
+++ src/components/ForgotPassword.js
@@ -1,6 +1,12 @@
-import React, { useState } from 'react'
 import { CognitoUser } from 'amazon-cognito-identity-js'
 import Pool from '../services/UserPool'
+import React, { useState } from 'react'
+import Box from '@mui/material/Box'
+import Grid from '@mui/material/Grid'
+import Button from '@mui/material/Button'
+import TextField from '@mui/material/TextField'
+import Container from '@mui/material/Container'
+import Typography from '@mui/material/Typography'
 
 export default function ForgotPassword (props) {
   const [stage, setStage] = useState(1) // 1: email stage; 2: code stage
@@ -40,45 +46,89 @@ export default function ForgotPassword (props) {
     }
 
     getUser().confirmPassword(code, password, {
-        onSuccess: data => {console.log('success', data)},
-        onFailure: err => {console.error('error', err)}
+      onSuccess: data => {
+        console.log('success', data)
+      },
+      onFailure: err => {
+        console.error('error', err)
+      }
     })
   }
   return (
-    <>
+    <Container component='main' maxWidth='xs'>
+      <Box
+        sx={{
+          marginTop: 8,
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center'
+        }}
+      >
       {stage === 1 && (
-        <form onSubmit={sendCode}>
-          <input
+        <Box component='form' onSubmit={sendCode} sx={{mt:1}}>
+          <TextField
             value={email}
             onChange={e => setEmail(e.target.value)}
             type='email'
             placeholder='Email'
+            margin='normal'
+            required
+            fullWidth
           />
-          <button type='submit'>Send Verification Code</button>
-        </form>
+          <Button
+            variant='contained'
+            type='submit'
+            fullWidth
+            sx={{ mt: 3, mb: 2 }}
+          >
+            Send Verification Code
+          </Button>
+        </Box>
       )}
       {stage === 2 && (
-        <form onSubmit={resetPassword}>
-          <input
+        <>
+        <Typography componen='h1' variant='h5'>
+        Please login below
+      </Typography>
+        <Box component='form' onSubmit={resetPassword} sx={{mt:1}}>
+          <TextField
             value={code}
             onChange={e => setCode(e.target.value)}
             placeholder='Verification Code'
+            margin='normal'
+            required
+            fullWidth
           />
-          <input
+          <TextField
             value={password}
             onChange={e => setPassword(e.target.value)}
             type='password'
             placeholder='New Password'
+            margin='normal'
+            required
+            fullWidth
           />
-          <input
+          <TextField
             value={confirmPassword}
             onChange={e => setConfirmPassword(e.target.value)}
             type='password'
             placeholder='Confirm Password'
+            margin='normal'
+            required
+            fullWidth
           />
-          <button type='submit'>Reset Password</button>
-        </form>
+          <Button
+            variant='contained'
+            type='submit'
+            fullWidth
+            sx={{ mt: 3, mb: 2 }}
+          >
+            Reset Password
+          </Button>
+        </Box>
+        </>
       )}
-    </>
+      </Box>
+    </Container>
   )
 }
