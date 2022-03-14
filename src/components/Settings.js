a30a6fbd0a391347fb0190aad78b5fb17527da5b
Michael Ndon <manj19.mjn@gmail.com>
2022-03-14 12:50:05 +0100

    style: modified input boxes

--- src/components/Settings.js
+++ src/components/Settings.js
@@ -1,15 +1,31 @@
 import React from 'react'
 import ChangePassword from './ChangePassword'
 import Header from './Header'
+import Container from '@mui/material/Container'
+import Box from '@mui/material/Box'
+import Typography from '@mui/material/Typography'
+import CssBaseline from '@mui/material/CssBaseline'
 
 export default function Settings ({ handleLogOut }) {
   return (
     <>
       <Header handleLogOut={handleLogOut} />
-      <div>
-        <h1>Settings</h1>
+      <Container component='main' maxWidth='xs'>
+      <CssBaseline />
+      <Box
+        sx={{
+          marginTop: 8,
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center'
+        }}
+      >
+        <Typography component='h1' variant='h5'>
+          Settings
+        </Typography>
         <ChangePassword />
-      </div>
+      </Box>
+      </Container>
     </>
   )
 }
